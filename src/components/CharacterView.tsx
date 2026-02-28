20861c1a4deceeb84dd14f446ffba1e0bb5b1d59
Randy Lutcavich <randy@randroid.dev>
2026-02-28 17:48:01 -0600

    Fix power button not visible on mobile (#36)

--- src/components/CharacterView.tsx
+++ src/components/CharacterView.tsx
@@ -5,7 +5,6 @@ import { GameCanvas } from './GameCanvas'
 import type { GameActions } from './GameCanvas'
 import { VisitorPanel } from './VisitorPanel'
 import { UpdateBanner } from './UpdateBanner'
-import { PowerButton } from './PowerButton'
 import { useCharacterPersistence } from '@/hooks/useCharacterPersistence'
 import { useLayoutPersistence } from '@/hooks/useLayoutPersistence'
 import type { CharacterState } from '@/lib/characterSchema'
@@ -108,7 +107,6 @@ export function CharacterView({ name }: CharacterViewProps) {
     <div style={{ display: 'flex', flexDirection: 'column', width: '100%', height: '100dvh' }}>
       <div style={{ flex: '1 1 0', minHeight: 0, position: 'relative' }}>
         {versionStale && <UpdateBanner />}
-        <PowerButton />
         <GameCanvas
           characterName={name}
           initialState={initialState}
